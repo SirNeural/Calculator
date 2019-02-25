64a4709d795503e0e33bc05d88b11170407c4c8c
SirNeural <13142366+SirNeural@users.noreply.github.com>
2019-02-24 23:22:43 -0700

    Initial Version

---
+++ Calculator.java
@@ -0,0 +1,82 @@
+/**
+ * Calculator class with addition, subtraction, multiplication, and division functions.
+ *
+ * @author Yun Hui Xu (1212530629)
+ * @version 2/24/2019
+ */
+
+package cse360assign2;
+
+public class Calculator {
+
+    private int total;
+
+    /**
+     * Constructor for Calculator class
+     * 
+     * @return void
+     */
+    public Calculator() {
+        total = 0; // not needed - included for clarity
+    }
+
+    /**
+     * Gets the total after any calculations done with the calculator
+     * 
+     * @return void
+     */
+    public int getTotal() {
+        return 0;
+    }
+
+    /**
+     * Adds the provided value with the total variable
+     * 
+     * @param value an integer to add in the calculator
+     * @return void
+     */
+    public void add(int value) {
+
+    }
+
+    /**
+     * Subtracts the provided value with the total variable
+     * 
+     * @param value an integer to subtract in the calculator
+     * @return void
+     */
+    public void subtract(int value) {
+
+    }
+
+    /**
+     * Multiplies the provided value with the total variable
+     * 
+     * @param value an integer to multiply in the calculator
+     * @return void
+     */
+    public void multiply(int value) {
+
+    }
+
+    /**
+     * Divides the total by the parameter, does integer division and if provided
+     * value is zero, the total after division will be set to 0. No exceptions will
+     * be raised.
+     * 
+     * @param value an integer to divide in the calculator
+     * @return void
+     */
+    public void divide(int value) {
+
+    }
+
+    /**
+     * Prints the history of actions done in the calculator
+     * 
+     * @return void
+     */
+    public String getHistory() {
+        return "";
+    }
+}
